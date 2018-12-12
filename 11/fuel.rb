b50edd2f2a2e30081c76317e1e410697c644f33b
Tobias Eilert <tobias.eilert@grandcentrix.net>
2018-12-12 16:08:35 +0100

    simplify nested each to #product

--- 11/fuel.rb
+++ 11/fuel.rb
@@ -11,10 +11,8 @@ end
 def calculate_grid
   power_levels = Hash.new(0)
 
-  (1..300).each do |y|
-    (1..300).each do |x|
-      power_levels[[x,y]] = cell_power_level(x, y)
-    end
+  (1..300).to_a.product((1..300).to_a).each do |x, y|
+    power_levels[[x,y]] = cell_power_level(x, y)
   end
 
   power_levels
@@ -23,13 +21,9 @@ end
 def calculate_squares(grid)
   squares = Hash.new(0)
 
-  (1..297).each do |y|
-    (1..297).each do |x|
-      (0..2).each do|xx|
-        (0..2).each do |yy|
-          squares[[x,y]] += grid[[x + xx, y + yy]]
-        end
-      end
+  (1..297).to_a.product((1..297).to_a).each do |x, y|
+    (0..2).to_a.product((0..2).to_a).each do |xx, yy|
+      squares[[x,y]] += grid[[x + xx, y + yy]]
     end
   end
 
